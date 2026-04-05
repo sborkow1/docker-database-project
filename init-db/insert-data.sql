d48d72c6f887acb470702be9c54e1356a8787c8b
sborkow1 <sborkow1@umbc.edu>
2026-04-05 19:48:02 -0400

    Update insert-data.sql

--- init-db/insert-data.sql
+++ init-db/insert-data.sql
@@ -1,13 +1,16 @@
 /*
-    Insert placeholder data into the User table
+    Insert data into the User table
 */
-INSERT INTO User 
-
+INSERT INTO User (UserName, UserPasswordHash, UserEmail)
+VALUES 
+('DrPool245', 'hG67AmG341hHJ5', 'DrPool245@yahoo.com'),
+('Mary98', 'TYp56g01Mm76', 'Mary98@yahoo.com'),
+('Tacofriend', '65AsPl7U8iB12', 'Tacofriend@yahoo.com');
 
 /*
-    Insert placeholder data in the the Subcription table
+    Insert data in the the Subcription table
 */
 
 /*
-    Insert placeholder data into the Video table
+    Insert data into the Video table
 */
