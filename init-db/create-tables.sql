30b4d8286d96c6ef091b101d39e3a4c7f7c9641f
sborkow1 <sborkow1@umbc.edu>
2026-04-05 16:56:03 -0400

    Update create-tables.sql

--- init-db/create-tables.sql
+++ init-db/create-tables.sql
@@ -3,62 +3,69 @@ CREATE DATABASE IF NOT EXISTS Billiards;
 USE Billiards;
 
 -- Dropping tables, just in case
-DROP TABLE IF EXISTS User;
-DROP TABLE IF EXISTS Subscription;
-DROP TABLE IF EXISTS Video;
+DROP TABLE IF EXISTS Downloads;
+DROP TABLE IF EXISTS Subscriptions;
+DROP TABLE IF EXISTS Videos;
+DROP TABLE IF EXISTS Plans;
+DROP TABLE IF EXISTS Users;
 
 /*
-    User table - stores information on user accounts
+    Users table - stores information on user accounts
 */
-CREATE TABLE User {
-    UserID INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Users (
+    UserID INT AUTO_INCREMENT,
     UserName VARCHAR(255) NOT NULL UNIQUE,
     UserPasswordHash VARCHAR(255) NOT NULL,
-    UserEmail VARCHAR(255) NOT NULL,
+    UserEmail VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY(UserID)
-}
+);
 
 /*
-    Subscription table - stores information on subscriptions
+    Plans table - stores the different available subscription plans
 */
-CREATE TABLE Subscription {
-    SubscriptionID INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Plans (
+    PlanID INT AUTO_INCREMENT,
+    PlanName VARCHAR(50) NOT NULL,
+    PlanDuration INT NOT NULL,
+    PlanPrice DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY(PlanID)
+);
+
+/*
+    Subscriptions table - stores information on user subscriptions
+*/
+CREATE TABLE Subscriptions (
+    SubscriptionID INT AUTO_INCREMENT,
     UserID INT NOT NULL,
-    SubscriptionPlan SET("Monthly", "Yearly"),
-    SubscriptionStartDate DATE NOT NULL,
-    SubscriptionEndDate DATE NOT NULL,
-    SubscriptionStatus SET("Active", "Expired", "Cancelled"),
+    PlanID INT NOT NULL,
+    SubscriptionStartDate DATETIME NOT NULL,
+    SubscriptionEndDate DATETIME NOT NULL,
     PRIMARY KEY (SubscriptionID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID)
-}
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (PlanID) REFERENCES Plans(PlanID)
+);
 
 /*
-    Match table - tracks the matches that take place at the venue
+    Videos table - stores information on recorded match videos
 */
-CREATE TABLE Match {
-    
-}
-
-
-/*
-    Video table - stores information on recorded match videos
-*/
-CREATE TABLE Video {
-    VideoID INT NOT NULL AUTO_INCREMENT,
-    VideoPath VARCHAR(255),
+CREATE TABLE Videos(
+    VideoID INT AUTO_INCREMENT,
+    VideoPath VARCHAR(255) NOT NULL,
+    VideoSize BIGINT NOT NULL,
+    VideoDate DATETIME NOT NULL,
     PRIMARY KEY(VideoID)
-}
+);
 
 /*
-    Download table - for tracking who downloads what and when
+    Downloads table - for tracking who downloads what and when
 */
-CREATE TABLE Download {
-    DownloadID INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Downloads(
+    DownloadID INT AUTO_INCREMENT,
     UserID INT NOT NULL,
     VideoID INT NOT NULL,
-    DownloadDate DATE NOT NULL,
-    DownloadIP VARCHAR(30) NOT NULL,
+    DownloadDate DATETIME NOT NULL,
+    DownloadIP VARCHAR(45) NOT NULL,
     PRIMARY KEY(DownloadID),
-    FOREIGN KEY UserID REFERENCES User(UserID),
-    FOREIGN KEY VideoID REFERENCES Video(VideoID)
-}
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (VideoID) REFERENCES Videos(VideoID)
+);
